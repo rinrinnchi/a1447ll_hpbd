21fd22b1c6374c307467188196ffd77a8527f874
Le Bao Linh <a1447ll@aiit.ac.jp>
2014-11-09 23:23:49 +0900

    Update a1447ll_hpbd.rb

--- lib/a1447ll_hpbd.rb
+++ lib/a1447ll_hpbd.rb
@@ -2,19 +2,18 @@ require "a1447ll_hpbd/version"
 require "a1447ll_hpbd/message"
 
 module A1447llHpbd
-  def self.run  
-    print "Input the someone's name who has birthday today: "
-    n = gets.chomp
-    s = n.upcase.split(%r{\s*})
-    name = ""
-    for i in 0 .. s.length-2 do
-      name = name + s[i] + "."
-    end
-    name = ".:" + name + s[-1] + ":."
-    msg = Message.new name
-    puts ""
-    msg.draw 
-    puts ""
-    return "Wish you a happy birthday! <3" 		
+  print "Input the someone's name who has birthday today: "
+  n = gets.chomp
+  s = n.upcase.split(%r{\s*})
+
+  name = ""
+  for i in 0 .. s.length-2 do
+    name = name + s[i] + "."
   end
+  name = ".:" + name + s[-1] + ":."
+
+  msg = Message.new name
+  puts ""
+  msg.draw 
+  puts "\n>> Wish you a happy birthday! <3" 		
 end
